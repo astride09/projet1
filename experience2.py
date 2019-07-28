b2e026fcbd55cc6a3abb5d19dea3f54c475fe28b
astride09 <45978637+astride09@users.noreply.github.com>
2019-07-28 18:18:20 +0200

    Add files via upload

--- experience2.py
+++ experience2.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential, Model
 from keras.layers.core import Dense, Dropout, Flatten
-from keras.layers import Concatenate
+from keras.layers import Concatenate, Reshape
 from keras.layers.convolutional import Conv2D
 from keras import optimizers
 from keras.optimizers import Adam
@@ -16,74 +16,6 @@ import argparse
 
 optimizer = Adam(0.0002, 0.5)
 
-# Create the model for right eye
-'''right_eye = Sequential()
-
-in1 = (None, None,1)
-right_eye.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in1, batch_input_shape = (None, None, None, 1)))
-right_eye.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-right_eye.add(MaxPooling2D(pool_size=(2, 2)))
-right_eye.add(Dropout(0.25))
-
-# Create the model for leftt eye
-left_eye = Sequential()
-
-in2 = (None, None,1)
-left_eye.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in2, batch_input_shape = (None, None,None, 1)))
-left_eye.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-left_eye.add(MaxPooling2D(pool_size=(2, 2)))
-left_eye.add(Dropout(0.25))
-
-# Create the model for right eyebrow
-right_eyebrow = Sequential()
-
-in3 = (None, None, 1)
-right_eyebrow.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in3, batch_input_shape = (None, None, None, 1)))
-right_eyebrow.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-right_eyebrow.add(MaxPooling2D(pool_size=(2, 2)))
-right_eyebrow.add(Dropout(0.25))
-
-
-# Create the model for leftt eyebrow
-left_eyebrow = Sequential()
-
-in4 = (None, None, 1)
-left_eyebrow.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in4, batch_input_shape = (None, None, None, 1)))
-left_eyebrow.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-left_eyebrow.add(MaxPooling2D(pool_size=(2, 2)))
-left_eyebrow.add(Dropout(0.25))
-
-
-'''
-# Create the model for mouth
-mouth = Sequential()
-
-in5 = (None, None, 1)
-mouth.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in5, batch_input_shape = (None, None, None, 1)))
-mouth.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-mouth.add(MaxPooling2D(pool_size=(3, 3)))
-mouth.add(Dropout(0.25))
-
-'''
-# Create the model for nose
-nose = Sequential()
-
-in6 = (None, None, 1)
-nose.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in6, batch_input_shape = (None, None, None, 1)))
-nose.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-nose.add(MaxPooling2D(pool_size=(2, 2)))
-nose.add(Dropout(0.25))
-
-'''
-# Create the model for jaw
-jaw = Sequential()
-
-in7 = (None, None, 1)
-jaw.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in7, batch_input_shape = (None, None, None, 1)))
-jaw.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-jaw.add(MaxPooling2D(pool_size=(2, 2)))
-jaw.add(Dropout(0.25))
-
 labels = np.ones((213),dtype='int64')
 
 labels[0:29]=0 #30
@@ -93,7 +25,7 @@ labels[91:121]=3 #31
 labels[122:151]=4 #30
 labels[152:182]=5 #31
 labels[183:]=6 #30
-
+print(labels)
 names = ['ANGRY','DISGUST','FEAR','HAPPY','NEUTRAL','SAD','SURPRISE']
 
 #load dataset
@@ -165,7 +97,7 @@ for dataset in data_path5:
     #print(data_path5)
     img_list5=os.listdir('/content/projet1/landmark/mouth'+'/'+ dataset)
     img_list5 = [img_list for img_list in img_list5 if img_list[-4:]=='.jpg']
-    print(img_list5)
+    #print(img_list5)
     for img in img_list5:
         input_img5=cv2.imread('/content/projet1/landmark/mouth' + '/'+ dataset + '/'+ img )
         input_img5=cv2.resize(input_img5, (150,150))
@@ -175,8 +107,23 @@ for dataset in data_path5:
 img_data5 = np.asarray(img_data_list5)
 img_data5 = img_data5.astype('float32')
 img_data5 = np.expand_dims(img_data5, -1)
-print(img_data5.shape)
+#print(img_data5.shape)
 xtrain5, xtest5,ytrain5,ytest5 = train_test_split(img_data5, labels,test_size=0.2,shuffle=True)
+xtrain5 = xtrain5.astype('float32')
+xtest5 = xtest5.astype('float32')
+
+xtrain5 /= 255
+xtest5 /= 255
+
+from keras.utils import to_categorical
+
+ytrain5 = to_categorical(ytrain5, 7)
+ytest5 = to_categorical(ytest5, 7)
+"""print(xtrain5)
+print('-------------------------------------------------')
+print(xtrain5.shape)
+print('-------------------------------------------------')
+print(ytrain5.shape)"""
 '''
 for dataset in data_path6:
     img_list6=os.listdir('./landmarks/lfw/nose'+'/'+ dataset)
@@ -194,7 +141,7 @@ xtrain6, xtest6,ytrain6,ytest6 = train_test_split(img_data6, labels_list,test_si
 for dataset in data_path7:
     img_list7=os.listdir('/content/projet1/landmark/mouth'+'/'+ dataset)
     img_list7 = [img_list for img_list in img_list7 if img_list[-4:]=='.jpg']
-    print(img_list7)
+    #print(img_list7)
     for img in img_list7:
         input_img7=cv2.imread('/content/projet1/landmark/mouth' + '/'+ dataset + '/'+ img )
         input_img7=cv2.resize(input_img7, (150,150))
@@ -204,20 +151,139 @@ for dataset in data_path7:
 img_data7 = np.asarray(img_data_list7)
 img_data7 = img_data7.astype('float32')
 img_data7 = np.expand_dims(img_data7, -1)
-print(img_data7.shape)
+#print(img_data7.shape)
 xtrain7, xtest7,ytrain7,ytest7 = train_test_split(img_data7, labels,test_size=0.2,shuffle=True)
 
+xtrain7 = xtrain7.astype('float32')
+xtest7 = xtest7.astype('float32')
+
+xtrain7 /= 255
+xtest7 /= 255
+
+from keras.utils import to_categorical
+
+ytrain7 = to_categorical(ytrain7, 7)
+ytest7 = to_categorical(ytest7, 7)
+
+######################################## Model #############################################
+# Create the model for right eye
+'''right_eye = Sequential()
+
+in1 = (None, None,1)
+right_eye.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in1, batch_input_shape = (None, None, None, 1)))
+right_eye.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+right_eye.add(MaxPooling2D(pool_size=(2, 2)))
+right_eye.add(Dropout(0.25))
+
+# Create the model for leftt eye
+left_eye = Sequential()
+
+in2 = (None, None,1)
+left_eye.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in2, batch_input_shape = (None, None,None, 1)))
+left_eye.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+left_eye.add(MaxPooling2D(pool_size=(2, 2)))
+left_eye.add(Dropout(0.25))
+
+# Create the model for right eyebrow
+right_eyebrow = Sequential()
+
+in3 = (None, None, 1)
+right_eyebrow.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in3, batch_input_shape = (None, None, None, 1)))
+right_eyebrow.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+right_eyebrow.add(MaxPooling2D(pool_size=(2, 2)))
+right_eyebrow.add(Dropout(0.25))
+
+
+# Create the model for leftt eyebrow
+left_eyebrow = Sequential()
+
+in4 = (None, None, 1)
+left_eyebrow.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in4, batch_input_shape = (None, None, None, 1)))
+left_eyebrow.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+left_eyebrow.add(MaxPooling2D(pool_size=(2, 2)))
+left_eyebrow.add(Dropout(0.25))
+
+
+'''
+# Create the model for mouth
+mouth = Sequential()
+
+#mouth.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in5))
+#mouth.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+#mouth.add(MaxPooling2D(pool_size=(2, 2), data_format=None))
+#mouth.add(Dropout(0.25))
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+
+mouth = Sequential()
+mouth.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=xtrain5.shape[1:]))
+mouth.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+mouth.add(MaxPool2D(pool_size=(2, 2)))
+mouth.add(Dropout(rate=0.25))
+mouth.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+mouth.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+mouth.add(MaxPool2D(pool_size=(2, 2)))
+mouth.add(Dropout(rate=0.25))
+mouth.add(Flatten())
+mouth.add(Dense(256, activation='relu'))
+print(mouth)
+mouth.add(Dropout(rate=0.5))
+print(mouth)
+mouth.add(Dense(7, activation='softmax'))
+
+
+
+'''
+# Create the model for nose
+nose = Sequential()
+
+in6 = (None, None, 1)
+nose.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=in6, batch_input_shape = (None, None, None, 1)))
+nose.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+nose.add(MaxPooling2D(pool_size=(2, 2)))
+nose.add(Dropout(0.25))
+
+'''
+# Create the model for jaw
+jaw = Sequential()
+jaw.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=xtrain5.shape[1:]))
+jaw.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+jaw.add(MaxPool2D(pool_size=(2, 2)))
+jaw.add(Dropout(rate=0.25))
+jaw.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+jaw.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+jaw.add(MaxPool2D(pool_size=(2, 2)))
+jaw.add(Dropout(rate=0.25))
+jaw.add(Flatten())
+jaw.add(Dense(256, activation='relu'))
+jaw.add(Dropout(rate=0.5))
+jaw.add(Dense(7, activation='softmax'))
+
+
+
+
 ################################Train Models#############################################
-mouth.compile(loss='categorical_crossentropy',
+from keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+  rotation_range=10,
+  zoom_range=0.1,
+  width_shift_range=0.1,
+  height_shift_range=0.1
+)
+
+
+mouth.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
-history1 = mouth.fit(xtrain5, ytrain5, batch_size=2, epochs=50, verbose=1)
+history1 = mouth.fit_generator(datagen.flow(xtrain5, ytrain5, batch_size=2), epochs=25,
+                              validation_data=(xtest5, ytest5), steps_per_epoch=xtrain5.shape[0]//2)
 
-jaw.compile(loss='categorical_crossentropy',
+jaw.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
-history2 = jaw.fit(xtrain5, ytrain5, batch_size=2, epochs=50, verbose=1)
-
+history2 = jaw.fit_generator(datagen.flow(xtrain7, ytrain7, batch_size=2), epochs=25,
+                              validation_data=(xtest7, ytest7), steps_per_epoch=xtrain7.shape[0]//2)
 
 
 
